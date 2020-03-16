099636fc456b26fc6498b237c62d31b53bdf6377
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-16 16:47:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+
+def batch_badge_creator[people]
+  new_array = []
+  people.collect do |people|
+    new_array << people
+  end
+  return new_array
+end
+
+
+def assign_rooms
+
+end
+
+
+
+def printer
+  
+end
